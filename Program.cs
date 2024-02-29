e0aa7068d5896956985a71dae9caff4398a4c238
lorenzo <lorenzo@LAPTOP-E8F845EH>
2024-02-29 08:33:21 +0100

    feat: Funzione StampaVotiPesi per stampare i vettori contenenti pesi e voti

--- Program.cs
+++ Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("inserisci il numero di voti:");
+            int nVoti = int.Parse(Console.ReadLine());
+            double[] voti = new double[nVoti];
+            int[] pesi = new int[nVoti];
+
+        }
+
+        void StampaVotiPesi(double[] voti, int[] pesi, int nVoti)
+        {
+            for(int i = 0;i < nVoti; i++)
+                Console.Write($"Voti   -   pesi \n --------------------- \n {voti[i]}                {pesi[i]}\n");
         }
     }
 }
