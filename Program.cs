b01ce922bff73628340e9df123eb7338bc9305ae
lorenzo <lorenzo@LAPTOP-E8F845EH>
2024-02-29 09:17:27 +0100

    feat: funzione media ponderata per trovare massimo e posizione massimo, minimo e posizione minimo, media dei voti

--- Program.cs
+++ Program.cs
@@ -6,17 +6,27 @@
         {
             Console.WriteLine("inserisci il numero di voti:");
             int nVoti = int.Parse(Console.ReadLine());
+
             double[] voti = new double[nVoti];
             int[] pesi = new int[nVoti];
+            double max = voti[0];
+            double min = voti[0];
+            int posmax = 0;
+            int posmin = 0;
 
             CaricaVettori(ref voti, ref pesi, nVoti);
             StampaVotiPesi(voti, pesi, nVoti);
+            double mediaPonderata = MediaPonderata(voti, pesi, ref max, ref posmax, ref min, ref posmin);
+            Console.WriteLine($"il voto massimo è {max} e la sua posizione è {posmax}");
+            Console.WriteLine($"il voto minimo è {min} e la sua posizione è {posmin}");
+            Console.WriteLine($"la media ponderata è {mediaPonderata}");
         }
 
         static void StampaVotiPesi(double[] voti, int[] pesi, int nVoti)
         {
-            for(int i = 0;i < nVoti; i++)
-                Console.Write($"Voti   -   pesi \n --------------------- \n {voti[i]}                {pesi[i]}\n");
+            Console.WriteLine("voti        pesi\n --------------------- \n");
+            for (int i = 0; i < nVoti; i++)
+                Console.Write($"{voti[i]}        {pesi[i]}\n");
         }
 
         static void CaricaVettori(ref double[] voti, ref int[] pesi, int nVoti)
@@ -28,5 +38,34 @@
                 pesi[i] = random.Next(1, 101);
             }
         }
+
+        static double MediaPonderata(double[] voti, int[] pesi, ref double max, ref int posmax, ref double min, ref int posmin)
+        {
+            double sommaVoti = 0;
+            int sommaPesi = 0;
+            double mediaPonderata;
+            for(int i = 0; i<voti.Length; i++) 
+            {
+                sommaVoti += voti[i];
+                sommaPesi += pesi[i];
+
+                if (voti[i] > max)
+                {
+                    max = voti[i];
+                    posmax = i;
+                }
+                if (voti[i] < min)
+                {
+                    min = voti[i];
+                    posmin = i;
+                }
+            }
+            mediaPonderata = sommaVoti / sommaPesi;
+            return mediaPonderata;
+        }
+
+
+
+
     }
 }
