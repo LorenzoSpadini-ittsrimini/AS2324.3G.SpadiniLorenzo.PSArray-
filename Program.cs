b8eabfe7ea7969f0bebb2d7924800a433decb831
lorenzo <lorenzo@LAPTOP-E8F845EH>
2024-02-29 08:39:04 +0100

    feat: Funzione carica vettori per caricare il vettore voti con valori casuali da 1 a 10 e il vettore pesi con valori casuali da 1 a 100

--- Program.cs
+++ Program.cs
@@ -9,12 +9,24 @@
             double[] voti = new double[nVoti];
             int[] pesi = new int[nVoti];
 
+            CaricaVettori(ref voti, ref pesi, nVoti);
+            StampaVotiPesi(voti, pesi, nVoti);
         }
 
-        void StampaVotiPesi(double[] voti, int[] pesi, int nVoti)
+        static void StampaVotiPesi(double[] voti, int[] pesi, int nVoti)
         {
             for(int i = 0;i < nVoti; i++)
                 Console.Write($"Voti   -   pesi \n --------------------- \n {voti[i]}                {pesi[i]}\n");
+        }
+
+        static void CaricaVettori(ref double[] voti, ref int[] pesi, int nVoti)
+        {
+            Random random = new Random();
+            for(int i = 0; i < nVoti; i++)
+            {
+                voti[i] = random.Next(1, 11);
+                pesi[i] = random.Next(1, 101);
+            }
         }
     }
 }
